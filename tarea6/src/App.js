498e313e2254660de126355d460761aab2c0206f
Rolymon <155546975+Rolymon@users.noreply.github.com>
2024-06-10 16:33:20 +0000

    acabao

--- tarea6/src/App.js
+++ tarea6/src/App.js
@@ -1,5 +1,6 @@
 import "./styles.css";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 //import './App.css';
 import { Header } from "./componentes/Header";
 import { Conversor } from "./componentes/Conversor";
@@ -7,6 +8,8 @@ import { Footer } from "./componentes/Footer";
 import { Section } from "./componentes/Section";
 import { Section1 } from "./componentes/Section1";
 import { Form } from "./componentes/Form";
+
+import {Carrucel} from  "./componentes/Carrucel";
 function App() {
   return (
     <div className="App">
@@ -15,6 +18,8 @@ function App() {
       <Conversor />
       <Section1 />
       <Form />
+     
+      <Carrucel/>
       <Footer />
     </div>
   );
---
+++ tarea6/src/componentes/Carrucel.js
@@ -0,0 +1,26 @@
+// Carrucel.js
+import React from "react";
+import '../styles/Carrucel.css'; // Importamos el archivo CSS
+import Img1 from "../imagenes/imagen1.jpg";
+import Img2 from "../imagenes/imagen2.jpg";
+import Img3 from "../imagenes/imagen3.jpg";
+import Img4 from "../imagenes/imagen3.jpg"; // Corregido
+import Img5 from "../imagenes/imagen2.jpg"; // Corregido
+import Img6 from "../imagenes/imagen1.jpg"; // Corregido
+
+export const Carrucel = () => {
+  return (
+    <div className="carysel-container">
+      <div className="carysel-grid">
+        <img src={Img1} className="carysel-item" alt="Imagen 1" />
+        <img src={Img2} className="carysel-item" alt="Imagen 2" />
+        <img src={Img3} className="carysel-item" alt="Imagen 3" />
+        <img src={Img4} className="carysel-item" alt="Imagen 4" />
+        <img src={Img5} className="carysel-item" alt="Imagen 5" />
+        <img src={Img6} className="carysel-item" alt="Imagen 6" />
+      </div>
+    </div>
+  );
+};
+
+
